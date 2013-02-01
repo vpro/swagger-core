ed82252641f98df3758c9c999f0d3dff30baedc5
Tony Tam <fehguy@gmail.com>
2013-01-31 20:13:50 -0500

    added form post example

--- samples/scala-jaxrs-fileupload/src/main/scala/com/wordnik/swagger/sample/resource/PetResource.scala
+++ samples/scala-jaxrs-fileupload/src/main/scala/com/wordnik/swagger/sample/resource/PetResource.scala
@@ -52,6 +52,30 @@ trait PetResource extends RestResourceUtil {
   }
 
   @POST
+  @Path("/formData")
+  @Consumes(Array(MediaType.MULTIPART_FORM_DATA))
+  @ApiOperation(value = "uploads a form data",
+    produces="application/json")
+  def formData(
+    @ApiParam(value = "user name") @FormDataParam("username") name: String,
+    @ApiParam(value = "user age") @FormDataParam("age") age: String) = {
+
+    val output = "thanks " + name + ", you're " + age + " years old"
+    Response.status(200).entity(new com.wordnik.swagger.sample.model.ApiResponse(200, output)).build()
+  }
+
+  // to keep swagger-ui happy, and support both html form and ajax FormData POST methods, we create
+  // another method which accepts application/x-www-form-urlencoded params, and calls the /formData
+  // method, which accepts multipart/form-data content.  Note that @ApiOperation is not in this method
+  // or it'd show as a duplicate in swagger-ui
+  @POST
+  @Path("/formData")
+  @Consumes(Array(MediaType.APPLICATION_FORM_URLENCODED))
+  def formData2(
+    @FormParam("username") name: String,
+    @FormParam("age") age: String) = formData(name, age)
+
+  @POST
   @Path("/uploadImage")
   @Consumes(Array(MediaType.MULTIPART_FORM_DATA))
   @ApiOperation(value = "uploads an image")
